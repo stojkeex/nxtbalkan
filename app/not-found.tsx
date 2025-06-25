00378455ae168e4e376fda7c53834f4a40e40a0f
stojkeex <stackflow.info@gmail.com>
2025-06-26 00:54:52 +0200

    not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center p-10">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-xl">Stran ni bila najdena.</p>
+    </div>
+  );
+}
